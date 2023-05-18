8b5f38f189dc752b2e00545379b1251543ae0a57
denys <dedpro88@gmail.com>
2023-05-18 12:33:19 +0200

    Second day
    Before menu

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,14 +1,21 @@
-(() => {
-  const openMenuBtnRef = document.querySelector('[data-menu-open-button]');
-  const closeMenuBtnRef = document.querySelector('[data-menu-close-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
-  const body = document.querySelector('body');
-  openMenuBtnRef.addEventListener('click', () => {
-    body.classList.toggle('hidden');
-    mobileMenuRef.classList.toggle('is-open');
-  });
-  closeMenuBtnRef.addEventListener('click', () => {
-    body.classList.toggle('hidden');
-    mobileMenuRef.classList.toggle('is-open');
-  });
-})();
+// Отримуємо посилання на кнопку меню та випадаюче меню
+var menuButton = document.querySelector('.menu');
+var dropdownMenu = document.querySelector('.dropdown-menu');
+
+// Додаємо подію кліку на кнопку меню
+menuButton.addEventListener('click', function (event) {
+  event.stopPropagation(); // Зупиняємо подальше поширення події кліку
+  dropdownMenu.style.display =
+    dropdownMenu.style.display === 'none' ? 'block' : 'none';
+});
+
+// Додаємо подію кліку до всього документу
+document.addEventListener('click', function (event) {
+  // Перевіряємо, чи клікнуто не на меню або його елементи
+  if (
+    !dropdownMenu.contains(event.target) &&
+    dropdownMenu.style.display === 'block'
+  ) {
+    dropdownMenu.style.display = 'none'; // Закриваємо меню
+  }
+});
